6e9f037ba4b581d8fac8996e538cdea79913b5a5
Akshay Dake <akshaydake343@gmail.com>
2018-04-06 16:42:21 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -143,14 +143,14 @@ MongoClient.connect(url, function(err, db1) {
 				   if(dem.trim() === dem1.trim() )
 				   {  
 				   */
-					//   demo+= " "+result.classroomno+ " " + "is alloted to professor "+ " " +result.faculty +" "+"who takes "+" "+result.subject +" " + "class" ;
+					  demo+= " "+result[0].classroomno+ " " + "is alloted to professor "+ " " +result[0].faculty +" "+"who takes "+" "+result[0].subject +" " + "class" ;
 	                           /*        counter++;
 				   } 
                }
 	}
 	*/
 	 //result="The total classrooms available are"+" " + counter + " "+ "and classroom number are " + " " + demo;
-        //result=demo;
+        result=demo;
 	 console.log(result);
    res.send(result);
    db1.close();
